786e047734e28a39c4fce6d712ae454340b612c8
Alex Kapajika <alex.kapajika.ntumba@stu.ocsb.ca>
2023-09-25 23:34:49 +0000

    Good morning, Ms Raffin

---
+++ area_per_rect_input.py
@@ -0,0 +1,9 @@
+def rectangle(length, width):
+    area = length * width
+    print("The area is", area, "units.")
+
+    perimeter = 2 * (length + width)
+    print("The perimeter is", perimeter, "units.")
+
+
+rectangle(5, 3)
